e31867846fd8496b1999d2ddb594ba5160a61d11
Peter Tribble <peter.tribble@gmail.com>
2019-10-30 08:53:02 +0000

    Update node to v6

--- node-fix.sh
+++ node-fix.sh
@@ -15,15 +15,18 @@ EOF
 #
 # we don't need all of this
 #
-rm -fr usr/versions/node-v4/include
-rm -fr usr/versions/node-v4/share
+rm -fr usr/versions/node-v6/include
+rm -fr usr/versions/node-v6/share
 # assume a 32-bit node binary
 rm -f usr/lib/amd64/libgcc_s.*
 rm -f usr/lib/amd64/libstdc++.so*
+rm -f usr/versions/node-v6/bin/amd64/node
+rm -f usr/versions/node-v6/bin/node
+mv usr/versions/node-v6/bin/i86/node usr/versions/node-v6/bin/node
 
 #
 # modify MRSIZE
 #
 TSIZE=`echo $MRSIZE | sed s:M::`
-TSIZE=$(($TSIZE+32))
+TSIZE=$(($TSIZE+36))
 MRSIZE=${TSIZE}M
